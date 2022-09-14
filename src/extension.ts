4bec2b494ca790fc12f992cccfe0a87bb6c6f4eb
所无聊 <2409277719@qq.com>
2022-09-14 09:43:48 +0800

    VSCode插件工程模板代码

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('vscode-extension.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World from vscode-extension!');
+	});
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
